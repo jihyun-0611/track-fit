23f7f5558ca5f34a0b3672ea03f8a9968668f2f9
jihyun-0611 <ahi0611@gmail.com>
2026-02-03 18:17:17 +0900

    [FEAT] Implement additional evaluation metrics in evaluation.py

--- protogcn/utils/evaluation.py
+++ protogcn/utils/evaluation.py
@@ -62,6 +62,26 @@ def confusion_matrix(y_pred, y_real, normalize=None):
     return confusion_mat
 
 
+def mean_class_accuracy(scores, labels):
+    """
+    Calculate mean class accuracy.
+    
+    Args:
+        scores (list[np.ndarray]): Prediction scores for each class.
+        labels (list[int]): Ground truth labels.
+    """
+    pred = np.argmax(scores, axis=1)
+    cf_mat = confusion_matrix(pred, labels).astype(float)
+
+    cls_cnt = cf_mat.sum(axis=1)
+    cls_hit = np.diag(cf_mat)
+
+    mean_class_acc = np.mean(
+        [hit / cnt if cnt else 0.0 for cnt, hit in zip(cls_cnt, cls_cnt)]
+    )
+    return mean_class_acc
+
+
 def top_k_accuracy(scores, labels, topk=(1, )):
     """
     Calculate  top_k_accuracy score
@@ -85,3 +105,70 @@ def top_k_accuracy(scores, labels, topk=(1, )):
 
     return res
 
+
+def mean_average_precision(scores, labels):
+    """
+    Mean average precision for multi-label recognition.
+
+    Args:
+        scores (list[np.ndarray]): Prediction scores of different classes for each sample.
+        labels (list[np.ndarray]): Ground truth many-hot vector for each sample.
+
+    Returns:
+        np.float : The mean average precision.
+    """
+    results = []
+    scores = np.stack(scores).T
+    labels = np.stack(labels).T
+
+    for score, label in zip(scores, labels):
+        precision, recall, _ = binary_precision_recall_curve(score, label)
+        ap = -np.sum(np.diff(recall) * np.array(precision)[:-1])
+        results.append(ap)
+    results = [x for x in results if not np.isnan(x)]
+    if results == []:
+        return np.nan
+    return np.mean(results)
+
+
+def binary_precision_recall_curve(y_score, y_true):
+    """Calculate the binary precision recall curve at step thresholds
+    
+    Args:
+        y_score (np.ndarray): Prediction scores for each class.
+            Shape should be (num_classes, ).
+        y_true (np.ndarray): Ground truth many-hot vector.
+            Shape shoud be (num_classes, ).
+
+    Returns:
+        precision (np.ndarray): The precision of different thresholds.
+        recall (np.ndarray): The recall of different thresholds.
+        thresholds (np.ndarray): Different thresholds at which precision and recall are tested.
+    """
+    assert isinstance(y_score, np.ndarray)
+    assert isinstance(y_true, np.ndarray)
+    assert y_score.shape == y_true.shape
+
+    # make y_true a boolean vector.
+    y_true = (y_true == 1)
+    # sort scores and corresponding truth labels.
+    desc_score_indices = np.argsort(y_score, kind='mergesort')[::-1]
+    y_score = y_score[desc_score_indices]
+    y_true = y_true[desc_score_indices]
+    # There may be ties in values, therefore find the `idstinct_value_inds`
+    distinct_value_inds = np.where(np.diff(y_score))[0]
+    threshold_inds = np.r_[distinct_value_inds, y_true.size-1]
+    # accumulate the true positives with decreasing threshold
+    tps = np.cumsum(y_true)[threshold_inds]
+    fps = 1+threshold_inds - tps
+    thresholds = y_score[threshold_inds]
+
+    precision = tps / (tps + fps)
+    precision[np.isnan(precision)] = 0
+    recall = tps / tps[-1]
+    # stop when full recall attained
+    # and reverse the outputs so recall is decreasing
+    last_ind = tps.searchsorted(tps[-1])
+    sl = slice(last_ind, None, -1)
+
+    return np.r_[precision[sl], -1], np.r_[recall[sl], 0], thresholds[sl]
