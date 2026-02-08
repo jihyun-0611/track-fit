7dc21d96aa90de2e889bb41ad8a501010bd382e3
jihyun-0611 <ahi0611@gmail.com>
2026-02-09 03:15:48 +0900

    [FEAT] implement pose dataset and dataset wrapper for repeated and concatenated datasets ( #11)

--- protogcn/datasets/pipelines/__init__.py
+++ protogcn/datasets/pipelines/__init__.py
@@ -1,3 +1,4 @@
 from compose import Compose
 from formatting import *
 from sampling import *
+from pose_related import *
--- protogcn/utils/utils.py
+++ protogcn/utils/utils.py
@@ -1,6 +1,7 @@
 import logging
 import os.path as osp
 import pickle
+import numpy as np
 
 
 def get_logger(name='protogcn', log_level=logging.INFO):
@@ -41,4 +42,29 @@ def dump_file(data, filepath):
             json.dump(data, f)
     else:
         raise ValueError(f'Unsupported file format: {filepath}')
+
+
+def auto_mix2(results_list, weights=None):
+    """Mix two modality  results with various weights.
+    
+    Args:
+        results_list: [rgb_results, pose_results], each is list of (num_classes,) arrays
+        weights: If None, try weights from 0.0 to 1.0 with step 0.05
+
+    Returns:
+        dict: {weight_str: mixed_results}
+    """
+    if weights is None:
+        weights = [i / 20.0 for i in range(21)] # 0.0, 0.05, ..., 1.0
+
+    rgb = np.array(results_list[0])
+    pose = np.array(results_list[1])
+
+    preds = {}
+    for w in weights:
+        mixed = w * rgb + (1-w) *pose
+        key = f'{w:.2f}'
+        preds[key] = list(mixed)
+
+    return preds
 
